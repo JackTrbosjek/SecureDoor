03bb425fc3c3fffbaa802d58c51f3cc684f410f7
Jakov <videkovic@uhp-digital.com>
2019-02-02 10:10:44 +0100

    Initial project commit

---
+++ Secure Door/Secure DoorTests/Secure_DoorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Secure_DoorTests.swift
+//  Secure DoorTests
+//
+//  Created by UHP Mac 5 on 02/02/2019.
+//  Copyright © 2019 Jakov. All rights reserved.
+//
+
+import XCTest
+@testable import Secure_Door
+
+class Secure_DoorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
